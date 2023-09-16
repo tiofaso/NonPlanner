6c82823c2cb87c43870df1c322a0d85524872546
Fábio Luiz Sousa Vaz <fabio.vaz@zup.com.br>
2023-09-16 12:13:27 -0300

    :contruction: arquivos iniciais

---
+++ src/main/java/com/catalisa/NonPlanner/NonPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.catalisa.NonPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catalisa/NonPlanner/NonPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalisa.NonPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
